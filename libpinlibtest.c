359edc3dbd799cc6dc342a46f6ff1cf9d80181e0
Brandon Lucia <blucia@gmail.com>
2021-09-10 07:48:25 -0400

    working v0 with outdated readme about tale of failure

--- libpinlibtest.c
+++ libpinlibtest.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stddef.h>
 
-extern "C" void rust_run(unsigned long);
+extern "C" unsigned long rust_run(unsigned long);
 extern "C" void run(unsigned long addr){
-  rust_run(addr);
+  fprintf(stderr, "addr = %lx\n",addr);
+  unsigned long res = rust_run(addr);
+  fprintf(stderr, "addr+1 = %lx\n",res);
+}
+
+extern "C" int bcmp(const void *b1, const void *b2, size_t length)
+{
+	char *p1, *p2;
+
+	if (length == 0)
+		return(0);
+	p1 = (char *)b1;
+	p2 = (char *)b2;
+	do
+		if (*p1++ != *p2++)
+			break;
+	while (--length);
+	return(length);
 }
--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -1,13 +1,12 @@
 #![no_std]
+#![no_builtins]
 
 use core::panic::PanicInfo;
 
 #[no_mangle]
-pub extern "C" fn rust_run(addr: i64) {
-
+pub extern "C" fn rust_run(addr: i64) -> i64{
+  addr+1
 }
-
-
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
